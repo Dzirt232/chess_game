cb1a129feadc1dc5dd193d61465151ac7a9a50aa
Dzirt232 <glorh823@gmail.com>
2017-11-15 22:51:18 +0300

    Added types for all chess.

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,28 +1,43 @@
 class Chess
-attr_accessor :position
+attr_accessor :type
+
 
   class King #король
 
+    def initialize
+      @type = "\u265A"
+    end
   end
 
   class Pawn #пешка
 
+    def initialize
+      @type = "\u265F"
+    end
   end
 
   class Knight #конь
-
+    def initialize
+      @type = "\u265E"
+    end
   end
 
   class Queen #королева
-
+    def initialize
+      @type = "\u265B"
+    end
   end
 
   class Bishop #слон
-
+    def initialize
+      @type = "\u265D"
+    end
   end
 
   class Rook #ладья
-
+    def initialize
+      @type = "\u265C"
+    end
   end
 
 end
