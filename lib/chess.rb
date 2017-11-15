4bd26e3f34b2e041a85b8cde1b9f0334ae94932d
Dzirt232 <glorh823@gmail.com>
2017-11-15 21:05:39 +0300

    Added class Chess.

---
+++ lib/chess.rb
@@ -0,0 +1,28 @@
+class Chess
+attr_accessor :position
+
+  class King #король
+
+  end
+
+  class Pawn #пешка
+
+  end
+
+  class Knight #конь
+
+  end
+
+  class Queen #королева
+
+  end
+
+  class Bishop #слон
+
+  end
+
+  class Rook #ладья
+
+  end
+
+end
