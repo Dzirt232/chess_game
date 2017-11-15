786114c30703b6332f38e72ddbbeb8513fdd2862
Dzirt232 <glorh823@gmail.com>
2017-11-15 20:25:45 +0300

    Added modules files and greeting in start.rb.

---
+++ start.rb
@@ -0,0 +1,17 @@
+print "Welcome to the chess game!!!\n"
+gets
+print "Enter a \"save\" in the game to saving.\n"
+gets
+while true
+  print "Choose a game mode:\n1: Player vs Player\n2: Player vs Computer\n"
+  variant = gets.chomp.strip.to_i
+  case variant
+  when 1
+    break
+  when 2
+    break
+  else
+    puts "You are enter a wrong answer! Try again..."
+    gets
+  end
+end
